0e93cc9e939600dfaf683f42f0eeab0cd796753c
kris52h6 <kris52h6@stud.kea.dk>
2021-09-24 17:44:47 +0200

    1st working iteration

--- src/Adventure.java
+++ src/Adventure.java
@@ -6,39 +6,114 @@ public class Adventure {
         Scanner scanner = new Scanner(System.in);
 
         // Create rooms
-        for (int i = 0; i < 9; i++) {
+        Room room1 = new Room("room1", "You're back where you started, at the dirt path.");
+        Room room2 = new Room("room2", "You're at a river, flowing with violent waves");
+        Room room3 = new Room("room3", "haunted house");
+        Room room4 = new Room("room4", "gloomy forest");
+        Room room5 = new Room("room5", "treasure room");
+        Room room6 = new Room("room6", "ass");
+        Room room7 = new Room("room7", "dark alley");
+        Room room8 = new Room("room8", "twisted bushes");
+        Room room9 = new Room("room9", "SPURGT MAND");
 
-        }
+        Room currentRoom = room1;
 
-        Room room1 = new Room();
-        Room room2 = new Room();
-        Room room3 = new Room();
-        Room room4 = new Room();
-        room1.setRoom(null, room2, room4, null);
+        room1.setConnections(null, room2, room4, null);
+        room2.setConnections(null, room3, null, room1);
+        room3.setConnections(null, null, room6, room2);
+        room4.setConnections(room1, null, room7, null);
+        room5.setConnections(null, null, room8, null);
+        room6.setConnections(room3, null, room9, null);
+        room7.setConnections(room4, room8, null, null);
+        room8.setConnections(room5, room9, null, room7);
+        room9.setConnections(room6, null, null, room8);
+
 
         // Intro
-        System.out.println("Welcome to the adventure game");
+        System.out.println("Welcome to the adventure game.");
+
 
         // Start
-        System.out.println("Which way do you want to go?");
+        System.out.println("\nYou wake up on the side of a dirt path, your only options are: to head east or: to head south.");
+        System.out.print("Which way do you want to go? ");
+
 
         // Move directions
         while (true) {
             String playerInput = scanner.nextLine();
+
+            // If player attempts to move north
             if (playerInput.equals("go north")) {
-                System.out.println("Going north");
-            } else if (playerInput.equals("go east")) {
-                System.out.println("Going east");
-            } else if (playerInput.equals("go south")) {
-                System.out.println("Going south");
-            } else if (playerInput.equals("go west")) {
-                System.out.println("Going west");
-            } else if (playerInput.equals("look")) {
-                System.out.println("Looking around"); // skal outputte beskrivelsen af currentRoom
+                if (currentRoom.getNorth() == null) {
+                    System.out.println("You cannot go this way");
+                } else {
+                    System.out.println("Going north");
+                    System.out.println(currentRoom.getNorth());
+                    currentRoom = currentRoom.getNorth();
+                    System.out.println(currentRoom.getDescription());
+                }
             }
+
+            // If player attempts to move east
+            if (playerInput.equals("go east")) {
+                if (currentRoom.getEast() == null) {
+                    System.out.println("You cannot go this way");
+                } else {
+                    System.out.println("Going east");
+                    System.out.println(currentRoom.getEast());
+                    currentRoom = currentRoom.getEast();
+                    System.out.println(currentRoom.getDescription());
+                }
+            }
+
+            // If player attempts to move south
+            if (playerInput.equals("go south")) {
+                if (currentRoom.getSouth() == null) {
+                    System.out.println("You cannot go this way");
+                } else {
+                    System.out.println("Going south");
+                    System.out.println(currentRoom.getSouth());
+                    currentRoom = currentRoom.getSouth();
+                    System.out.println(currentRoom.getDescription());
+                }
+            }
+
+            // If player attempts to move west
+            if (playerInput.equals("go west")) {
+                if (currentRoom.getWest() == null) {
+                    System.out.println("You cannot go this way");
+                } else {
+                    System.out.println("Going west");
+                    System.out.println(currentRoom.getWest());
+                    currentRoom = currentRoom.getWest();
+                    System.out.println(currentRoom.getDescription());
+                }
+            }
+
+            // If player looks
+            if (playerInput.equals("look")) {
+                System.out.println(currentRoom.getDescription());
+            }
+
+            if (playerInput.equals("exit")) {
+                System.out.println("Killing program...");
+                System.exit(0);
+            }
+
+            if (playerInput.equals("help")) {
+                System.out.println("\nWelcome to the help menu:");
+                System.out.println("\tType 'go north', to go north");
+                System.out.println("\tType 'go east', to go east");
+                System.out.println("\tType 'go south', to go south");
+                System.out.println("\tType 'go west', to go west");
+                System.out.println("\tType 'look', to get a description of the room your currently in");
+                System.out.println("\tType 'exit' to quit the program");
+                System.out.println("\t- Good luck :)");
+            }
+
+        }
+
         }
 
 
     }
-
-}
--- src/Room.java
+++ src/Room.java
@@ -1,17 +1,54 @@
 public class Room {
+    private String name;
     private Room north;
     private Room east;
     private Room south;
     private Room west;
+    private String description;
 
-        public Room() {
+        public Room(String name, String description) {
+            this.name = name;
+            this.description = description;
         }
 
-        public void setRoom(Room north, Room east, Room south, Room west) {
+        public void setConnections(Room north, Room east, Room south, Room west) {
             this.north = north;
             this.east = east;
             this.south = south;
             this.west = west;
         }
 
+        public void connectRoomsEast(Room nextRoom) {
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public Room getNorth() {
+            return north;
+        }
+
+        public Room getEast() {
+            return east;
+        }
+
+        public Room getSouth() {
+            return south;
+        }
+
+        public Room getWest() {
+            return west;
+        }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
