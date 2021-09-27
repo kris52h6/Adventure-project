792a7d2d77fc79996a948a21d4bb622d24a351b0
kris52h6 <kris52h6@stud.kea.dk>
2021-09-27 12:03:13 +0200

    Adventure v1 - (if changed to switch) - player obj not working

--- src/Adventure.java
+++ src/Adventure.java
@@ -4,6 +4,8 @@ public class Adventure {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String playerInput = "";
+
 
         // Intro
         System.out.println("Welcome to the adventure game.");
@@ -18,76 +20,79 @@ public class Adventure {
         Map map = new Map();
         Room currentRoom = map.getCurrentRoom();
 
-        // Move directions
+/*        Player player = new Player(currentRoom);
+        System.out.println(player.getCurrentRoom());*/
+
+
+        // Player move / interaction
         while (true) {
-            String playerInput = scanner.nextLine();
-            // If player attempts to move north
-            if (playerInput.equals("go north")) {
-                if (currentRoom.getNorth() == null) {
-                    System.out.println("You cannot go this way");
-                } else {
-                    System.out.println("Going north");
-                    System.out.println(currentRoom.getNorth());
-                    currentRoom = currentRoom.getNorth();
-                    System.out.println(currentRoom.getDescription());
+            playerInput = scanner.nextLine();
+
+            switch (playerInput) {
+                case "go north", "north", "n" -> {
+                    if (currentRoom.getNorth() == null) {
+                        System.out.println("\nYou cannot go this way");
+                    }
+                    else {
+                        System.out.println("\nGoing north");
+                        currentRoom = currentRoom.getNorth();
+                        System.out.println(currentRoom.getDescription());
+                    }
                 }
-            }
-
-            // If player attempts to move east
-            if (playerInput.equals("go east")) {
-                if (currentRoom.getEast() == null) {
-                    System.out.println("You cannot go this way");
-                } else {
-                    System.out.println("Going east");
-                    System.out.println(currentRoom.getEast());
-                    currentRoom = currentRoom.getEast();
-                    System.out.println(currentRoom.getDescription());
+                case "go east", "east", "e" -> {
+                    if (currentRoom.getEast() == null) {
+                        System.out.println("\nYou cannot go this way");
+                    } else {
+                        System.out.println("\nGoing east");
+                        currentRoom = currentRoom.getEast();
+                        System.out.println(currentRoom.getDescription());
+                    }
                 }
-            }
 
-            // If player attempts to move south
-            if (playerInput.equals("go south")) {
-                if (currentRoom.getSouth() == null) {
-                    System.out.println("You cannot go this way");
-                } else {
-                    System.out.println("Going south");
-                    System.out.println(currentRoom.getSouth());
-                    currentRoom = currentRoom.getSouth();
-                    System.out.println(currentRoom.getDescription());
+                case "go south", "south", "s" -> {
+                    if (currentRoom.getSouth() == null) {
+                        System.out.println("\nYou cannot go this way");
+                    } else {
+                        System.out.println("\nGoing south");
+                        currentRoom = currentRoom.getSouth();
+                        System.out.println(currentRoom.getDescription());
+                    }
                 }
-            }
 
-            // If player attempts to move west
-            if (playerInput.equals("go west")) {
-                if (currentRoom.getWest() == null) {
-                    System.out.println("You cannot go this way");
-                } else {
-                    System.out.println("Going west");
-                    System.out.println(currentRoom.getWest());
-                    currentRoom = currentRoom.getWest();
-                    System.out.println(currentRoom.getDescription());
+                case "go west", "west", "w" -> {
+                    if (currentRoom.getWest() == null) {
+                        System.out.println("\nYou cannot go this way");
+                    } else {
+                        System.out.println("\nGoing west");
+                        currentRoom = currentRoom.getWest();
+                        System.out.println(currentRoom.getDescription());
+                    }
                 }
-            }
 
-            // If player looks
-            if (playerInput.equals("look")) {
-                System.out.println(currentRoom.getDescription());
-            }
+                // if player looks
+                case "look", "looking" -> {
+                    System.out.println(currentRoom.getDescription());
 
-            if (playerInput.equals("exit")) {
-                System.out.println("Killing program...");
-                System.exit(0);
-            }
+                }
 
-            if (playerInput.equals("help")) {
-                System.out.println("\nWelcome to the help menu:");
-                System.out.println("\tType 'go north', to go north");
-                System.out.println("\tType 'go east', to go east");
-                System.out.println("\tType 'go south', to go south");
-                System.out.println("\tType 'go west', to go west");
-                System.out.println("\tType 'look', to get a description of the room your currently in");
-                System.out.println("\tType 'exit' to quit the program");
-                System.out.println("\t- Good luck :)");
+                // exit program
+                case "exit", "quit" -> {
+                    System.out.println("Killing program...");
+                    System.exit(0);
+                }
+
+                // If player types help
+                case "help", "h", "info" -> {
+                    System.out.println("\nWelcome to the help menu:");
+                    System.out.println("\tType 'go north', to go north");
+                    System.out.println("\tType 'go east', to go east");
+                    System.out.println("\tType 'go south', to go south");
+                    System.out.println("\tType 'go west', to go west");
+                    System.out.println("\tType 'look', to get a description of the room your currently in");
+                    System.out.println("\tType 'exit' to quit the program");
+                    System.out.println("\t- Good luck :)\n");
+                }
+
             }
 
         }
---
+++ src/Player.java
@@ -0,0 +1,13 @@
+/*public class Player {
+    private Room currentRoom;
+
+
+    public Player(Room currentRoom) {
+        this.currentRoom = currentRoom;
+    }
+
+    public Room getCurrentRoom() {
+        return currentRoom;
+    }
+
+}*/
